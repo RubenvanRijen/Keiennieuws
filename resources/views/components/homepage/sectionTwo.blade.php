26feb81a8b60de550462cb277b3598a6531959c3
rgjrijen <ybK62Hm&>
2022-03-05 11:52:21 +0100

    sectionTwo part One done

---
+++ resources/views/components/homepage/sectionTwo.blade.php
@@ -0,0 +1,9 @@
+<div class="bg-image p-5 text-center shadow-1-strong rounded mb-5 text-white">
+    <h1 class="mb-3 h2">Jumbotron</h1>
+
+    <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus praesentium
+        labore accusamus sequi, voluptate debitis tenetur in deleniti possimus modi voluptatum
+        neque maiores dolorem unde? Aut dolorum quod excepturi fugit.
+    </p>
+</div>
