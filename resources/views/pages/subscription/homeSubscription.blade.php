6a957cebf6b9c457aca1b9480fecbeec654fa08f
rgjrijen <ybK62Hm&>
2022-04-09 21:09:37 +0200

    home subscription done

--- resources/views/pages/subscription/homeSubscription.blade.php
+++ resources/views/pages/subscription/homeSubscription.blade.php
@@ -10,14 +10,29 @@
         <div class="row">
             <div class="col">
                 <a href="/subscription/start" class="btn btn-warning title-text">Starten</a>
+                <div class="card">
+                    <div class="card-body">
+                        <p class="card-text">Wanneer u een abonnement start dan is dat voor onbepaalde tijd. Een abonnement op het Keiennieuws kost per editie
+                            1,50 euro. Elk jaar levert het keiennieuws 10 edities op. Dit begint in september en de laatste
+                            editie van het jaar is in juni.</p>
+                    </div>
+                </div>
             </div>
             <div class="col">
-                <a href="/subscription/end" class="btn btn-warning title-text">Eindigen
-                </a>
+                <a href="/subscription/end" class="btn btn-warning title-text">Eindigen </a>
+                <div class="card">
+                    <div class="card-body">
+                        <p class="card-text">Wanneer u geen interesse meer heeft in het Keiennieuws, dan moet u dit voor de 25ste van de maand beëindigen.</p>
+                    </div>
+                </div>
             </div>
             <div class="col">
-                <a href="/subscription/edit" class="btn btn-warning title-text">Wijzigen
-                </a>
+                <a href="/subscription/edit" class="btn btn-warning title-text">Wijzigen </a>
+                <div class="card">
+                    <div class="card-body">
+                        <p class="card-text">Bent u verhuisd? Of heeft u een nieuw mailadres? geef dit kosteloos aan ons door.</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
