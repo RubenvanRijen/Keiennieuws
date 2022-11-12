87d8f77de70a8a5ef01aa3b91b297357e824798a
rgjrijen <58332716+RubenvanRijen@users.noreply.github.com>
2022-11-12 12:58:04 +0100

    sidebar for admin made

--- resources/views/components/layouts/verticalSideBar.blade.php
+++ resources/views/components/layouts/verticalSideBar.blade.php
@@ -4,7 +4,7 @@
              <li class="nav-item">
                  <a href="/dashboard/person-information" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
                      <div class="row">
-                         <i class="fs-1 bi bi-person-square text-dark"></i>
+                         <i class="fs-1 bi bi-person-lines-fill text-dark"></i>
                          <span class="ms-1 d-none d-sm-inline text-dark">Gegevens</span>
                      </div>
                  </a>
@@ -26,11 +26,34 @@
                  </a>
              </li>
          </ul>
-
          @hasrole('admin')
          <hr style="border: 1px #6c757d solid !important; opacity: 1; width: 100%; margin: auto; margin-top: 5%; margin-bottom: 5%;">
-
+         <ul class="nav nav-pills nav-flush flex-md-column flex-row flex-nowrap mb-auto mx-auto text-center align-items-center">
+             <li class="nav-item">
+                 <a href="/dashboard/person-information" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
+                     <div class="row">
+                         <i class="fs-1 bi bi-person-square text-dark"></i>
+                         <span class="ms-1 d-none d-sm-inline text-dark">Gebruikers</span>
+                     </div>
+                 </a>
+             </li>
+             <li>
+                 <a href="/dashboard/person-reservations" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
+                     <div class="row">
+                         <i class="fs-1 bi bi-newspaper text-dark"></i>
+                         <span class="ms-1 d-none d-sm-inline text-dark">Edities</span>
+                     </div>
+                 </a>
+             </li>
+             <li>
+                 <a href="/dashboard/person-security" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
+                     <div class="row">
+                         <i class="fs-1 bi bi-book-fill text-dark"></i>
+                         <span class="ms-1 d-none d-sm-inline text-dark">Bookingen</span>
+                     </div>
+                 </a>
+             </li>
+         </ul>
          @endhasrole
-
      </div>
  </div>
