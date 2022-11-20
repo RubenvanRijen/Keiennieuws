fe438626a71e54539f8c6658673bc6320f162c6f
rgjrijen <58332716+RubenvanRijen@users.noreply.github.com>
2022-11-20 21:31:37 +0100

    ignore the zips and edit sectionblade

--- resources/views/components/homepage/sectionTwo.blade.php
+++ resources/views/components/homepage/sectionTwo.blade.php
@@ -3,8 +3,8 @@
         <div class="card">
             <div class="card-body">
                 <h5 class="card-title">NOG <span class="number-days">{{$timeDiff}}</span> DAGEN</h5>
-                <p class="card-text">Wilt u iets in het Keiennieuws plaatsen? @if ($edition !== null)Dan heeft u tot de tijd om uw materiaal bij ons aan te leveren voor in de editie van
-                    {{$edition->title}} {{$edition->endDateUpload}} @else er is op het moment nog geen edite beschikbaar @endif . <br>Wilt u meteen een plek reserveren en plaatsen, kies dan aanleveren. Wilt u later uw bestanden aanleveren maar wel verzekerd zijn van een plek kies dan reserveren.
+                <p class="card-text">Wilt u iets in het Keiennieuws plaatsen? @if ($edition !== null)Dan heeft u tot {{$edition->endDateUpload}} de tijd om uw materiaal bij ons aan te leveren voor in de editie van
+                    {{$edition->title}} @else er is op het moment nog geen edite beschikbaar @endif . <br>Wilt u meteen een plek reserveren en plaatsen, kies dan aanleveren. Wilt u later uw bestanden aanleveren maar wel verzekerd zijn van een plek kies dan reserveren.
                 </p>
                 <div class="row">
                     <div class="col mt-2">
